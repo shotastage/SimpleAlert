f46d68cf5f5a19c3ffe9c732a8cdd86cf3a7b706
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-15 10:40:26 +0900

    Initial Commit

---
+++ SimpleAlert/SimpleAlert.h
@@ -0,0 +1,19 @@
+//
+//  SimpleAlert.h
+//  SimpleAlert
+//
+//  Created by Shota Shimazu on 2020/02/15.
+//  Copyright © 2020 Shota Shimazu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleAlert.
+FOUNDATION_EXPORT double SimpleAlertVersionNumber;
+
+//! Project version string for SimpleAlert.
+FOUNDATION_EXPORT const unsigned char SimpleAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleAlert/PublicHeader.h>
+
+
