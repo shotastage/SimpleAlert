e95df91b782281c45d7cef20bd9765115d5333d2
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-25 15:10:15 +0900

    add example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Shota Shimazu on 2020/02/25.
+//  Copyright © 2020 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
