d126fa3abb3b6bee1a5391c960fe0428882bcd8e
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-25 15:37:53 +0900

    add basic code

--- SimpleAlert/SimpleAlert.swift
+++ SimpleAlert/SimpleAlert.swift
@@ -10,77 +10,40 @@ import UIKit
 
 
 
-class SimpleDialogController: UIPresentationController {
-    
-    private let overlayView = UIView()
-
-}
-
-class SimpleDialog {
-
-    
-    static func confirm(message: String) -> UIAlertController {
-        return self.show(title: "Confirm", message: message)
-    }
-    
-    static func show(title: String, message: String) -> UIAlertController {
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: UIAlertController.Style.alert
-        )
-        
-        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
-            (action: UIAlertAction!) -> Void in
-        })
-        
-        alert.addAction(defaultAction)
-
-        return alert
-    }
-}
-
-
-
-@available(iOS, introduced: 10.0, deprecated: 12.0, renamed: "SimpleDialog")
 open class SimpleAlert {
     
-    var title: String!
-    var message: String!
+    var title: String?
     
+    var message: String?
+}
+
+
+protocol Buildable {
+    associatedtype BuildType
+ 
+    func build() -> SimpleAlert
+}
+
+
+class Builder: Buildable {
+
+    private var title: String?
     
-    public func title(_ t: String) {
-        self.title = t
+    private var message: String?
+    
+    func title(_ title: String) -> Self {
+        return self
     }
     
-    public func show() {
-        let alert: UIAlertController = UIAlertController(title: self.title, message: self.message, preferredStyle:  UIAlertController.Style.alert)
-        
-        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
-            (action: UIAlertAction!) -> Void in
-        })
-        
-        alert.addAction(defaultAction)
-        
-        UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+    func message(_ message: String) -> Self {
+        return self
     }
+
+    typealias BuildType = SimpleAlert
     
-    static public func confirm(message: String) {
+    func build() -> SimpleAlert {
+        let alert = SimpleAlert()
         
-        let alert: UIAlertController = UIAlertController(title: message, message: "", preferredStyle:  UIAlertController.Style.alert)
-        
-        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
-            (action: UIAlertAction!) -> Void in
-        })
-        
-        alert.addAction(defaultAction)
-        
-        
-        
-        // - UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
-        
-        
-        
-        UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+        return alert
     }
 }
